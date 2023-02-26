5038df484575a4b32f4cc888409331bf8f9032ce
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2023-02-26 11:13:44 +0000

    [END]完成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -22,6 +22,7 @@ class Admin::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = '投稿を削除しました'
     redirect_to admin_posts_path
   end
 
