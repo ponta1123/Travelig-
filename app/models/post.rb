988408301edabc7c0c976bd80f067da9955705d4
EC2 Default User <ec2-user@ip-172-31-10-121.ap-northeast-1.compute.internal>
2023-03-22 13:37:33 +0000

    [add]imageバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :image
+  validates :image, presence: true
   belongs_to :user
   has_many :post_tags, dependent: :destroy
   has_many :tags,through: :post_tags
